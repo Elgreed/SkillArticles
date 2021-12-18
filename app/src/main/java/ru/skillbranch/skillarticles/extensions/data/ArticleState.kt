9e367ae27f51d31fd294e1f3d1ec4b2912257b76
Коротчик Сергей <skorotchik@corp.local>
2021-12-18 22:24:31 +0300

    домашнее задание

--- app/src/main/java/ru/skillbranch/skillarticles/extensions/data/ArticleState.kt
+++ app/src/main/java/ru/skillbranch/skillarticles/extensions/data/ArticleState.kt
@@ -4,10 +4,3 @@ import ru.skillbranch.skillarticles.data.AppSettings
 import ru.skillbranch.skillarticles.data.ArticlePersonalInfo
 import ru.skillbranch.skillarticles.viewmodels.ArticleState
 
-fun ArticleState.toAppSettings() : AppSettings {
-    return AppSettings(isDarkMode,isBigText)
-}
-
-fun ArticleState.toArticlePersonalInfo(): ArticlePersonalInfo {
-    return ArticlePersonalInfo(isLike, isBookmark)
-}
