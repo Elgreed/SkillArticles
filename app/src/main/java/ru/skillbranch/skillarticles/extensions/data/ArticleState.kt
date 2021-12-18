1a3ebf967b5301f71f5a72e7bb91c7611f37f191
Коротчик Сергей <skorotchik@corp.local>
2021-12-18 22:33:09 +0300

    домашнее задание

--- app/src/main/java/ru/skillbranch/skillarticles/extensions/data/ArticleState.kt
+++
@@ -1,6 +0,0 @@
-package ru.skillbranch.skillarticles.extensions.data
-
-import ru.skillbranch.skillarticles.data.AppSettings
-import ru.skillbranch.skillarticles.data.ArticlePersonalInfo
-import ru.skillbranch.skillarticles.viewmodels.ArticleState
-
